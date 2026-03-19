aee37304f6a637dc93aafdeeb8094ee54a7ab4c7
Brayden Carlson <braycarlson@hotmail.com>
2026-03-18 22:26:52 -0600

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,12 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 5173,
+        proxy: {
+            '/api': {
+                target: process.env.API_URL || 'http://localhost:8080',
+                changeOrigin: true,
+            },
+        },
         watch: {
             usePolling: true,
         },
